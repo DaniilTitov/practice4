146f27406f77490ef20fea254c76797f7f801a1d
Daniil Titov <Daniil.A.Titov@gmail.com>
2020-04-28 20:05:42 +0300

    added name regexp refactoring

--- src/main/java/technology/bear/Parser.java
+++ src/main/java/technology/bear/Parser.java
@@ -1,13 +1,48 @@
 package technology.bear;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
+import static java.util.Arrays.stream;
+
 public class Parser {
     public static final String PHONE_NUMBER_REGEXP = "(\\+[0-9]+)?[(]?[0-9]{1,4}[)]?[-\\s./0-9]*[0-9]";
+    public static final String NAME_REGEXP = "(?!^)(?<!\\. )[А-Я][а-я]+";
+    public static final String REPLACEMENT = "[censored]";
 
-    public static final String input = "Всем привет. Меня зовут Даниил, сегодня бла бла бла. " +
-            "Мой друг Николай звонит по телефону +79999999999";
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        censorText("война_и_мир.txt");
+        censorText("номера_телефонов.txt");
+    }
 
-    public static void main(String[] args) {
-        String fixedInput = input.replaceAll(PHONE_NUMBER_REGEXP, "[censored]");
-        System.out.println(fixedInput);
+    private static void censorText(String textName) throws IOException, URISyntaxException {
+        URL resource = Parser.class.getResource("/" + textName);
+        String content = readFile(get(resource.toURI()).toFile().toString(), UTF_8);
+
+        System.out.println(textName + "\nИсходный текст:");
+        System.out.println(content);
+
+        System.out.println("После преобразования:");
+        System.out.println(censorText(content, REPLACEMENT, PHONE_NUMBER_REGEXP, NAME_REGEXP) + "\n");
+    }
+
+    private static String readFile(String path, Charset encoding) throws IOException {
+        return new String(readAllBytes(get(path)), encoding);
+    }
+
+    private static String censorText(String text, String replacement, String... regexps) {
+        if (regexps == null || regexps.length == 0) {
+            return "";
+        }
+        StringBuilder regexpBuilder = new StringBuilder("(");
+        stream(regexps).forEach(r -> regexpBuilder.append(r).append("|"));
+        regexpBuilder.deleteCharAt(regexpBuilder.length() - 1);
+        regexpBuilder.append(")");
+        return text.replaceAll(regexpBuilder.toString(), replacement);
     }
 }
